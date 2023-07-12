f929986d3e5e5a8b4b0484ad1b89a2917a9d9221
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-07-12 19:20:54 +0300

    console create command fix

--- console.py
+++ console.py
@@ -4,31 +4,34 @@ import cmd
 from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
-	prompt = '(hbnb) '
+    prompt = '(hbnb) '
+    __classes = {"BaseModel"}
 
-	def do_quit(self, arg):
-		"""Exit the program"""
-		return True
+    def do_quit(self, arg):
+        """Exit the program"""
+        return True
     
-	def do_EOF(self, arg):
-		"""Exit the program"""
-		return True
+    def do_EOF(self, arg):
+        """Exit the program"""
+        return True
 
-		
-	def do_help(self, arg):
-		"""List available commands"""
-		cmd.Cmd.do_help(self, arg)
+    def do_help(self, arg):
+        """List available commands"""
+        cmd.Cmd.do_help(self, arg)
 
-	def emptyline(self):
-		pass
-	def do_create(self, arg):
-		"""create a new class instance and print"""
-		if len(arg) == 0:
-			print("** class name missing **")
-		elif arg[0] not in HBNBCommand:
-			print("** class doesn't exist **")
-		else:
-			print(arg[0]().id)
+    def emptyline(self):
+        pass
+
+    def do_create(self, arg):
+        """create a new class instance and print"""
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(args[0])().id)
 			
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
