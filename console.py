11bd2f6d12f154e5429fe7a9e3c495c2c34d554d
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-07-14 14:40:42 +0300

    adding and updating the console commands

--- console.py
+++ console.py
@@ -2,10 +2,12 @@
 
 import cmd
 from models.base_model import BaseModel
+from models.__init__ import storage
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
-    __classes = {"BaseModel"}
+    __classes = {"BaseModel"
+                 }
 
     def do_quit(self, arg):
         """Exit the program"""
@@ -32,6 +34,114 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
         else:
             print(eval(args[0])().id)
-			
+            storage.save()
+
+    def do_show(self, arg):
+        """
+        Prints representation of an instance based on class name and id
+        """
+        args = arg.split()
+        obj_dict = storage.all()
+        
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(args[0], args[1]) not in obj_dict:
+            print("** no instance found **")
+        else:
+            print(obj_dict['{}.{}'.format(args[0], args[1])])
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id
+        """
+        args = arg.split()
+        obj_dict = storage.all()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif '{}.{}'.format(args[0], args[1]) not in obj_dict:
+            print("** no instance found **")
+        else:
+            del obj_dict['{}.{}'.format(args[0], args[1])]
+            storage.save()
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances
+        based or not on the class name
+        """
+        args = arg.split()
+
+        if len(args) > 0 and args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            all_instances = []
+
+            if len(args) == 0:
+                instances = storage.all().values()
+            else:
+                instances = [v for v in storage.all().values() if v.__class__.__name__ == args[0]]
+
+                for instance in instances:
+                    all_instances.append(str(instance))
+                print(all_instances)
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id
+        adding or updating attribute
+        """
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        else:
+            class_name = args[0]
+
+            if class_name not in self.__classes:
+                print("** class doesn't exist **")
+            elif len(args) == 1:
+                print("** instance id missing **")
+            else:
+                instance_id = args[1]
+                instances = storage.all()
+                
+                instance_key = '{}.{}'.format(class_name, instance_id)
+                if instance_key not in instances:
+                    print("** no instance found **")
+                elif len(args) == 2:
+                    print("** attribute name missing **")
+                elif len(args) == 3:
+                    print("** value missing **")
+                else:
+                    attr_name = args[2]
+                    attr_value = args[3]
+                    attr_value = attr_value.replace('"', '')
+                    instance = instances[instance_key]
+
+                    if hasattr(instance, attr_name):
+                        setattr(instance, attr_name, attr_value)
+                    else:
+                        setattr(instance, attr_name, attr_value)
+                        storage.save()
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
