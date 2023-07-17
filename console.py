1a96e5e8fc70402a48b1d0a2b1efa3ce14e0163d
WinnyKay3 <winffredkalondu@gmail.com>
2023-07-17 04:32:26 -0700

    console.py

--- console.py
+++ console.py
@@ -4,17 +4,18 @@ import cmd
 
 from models.base_model import BaseModel
 from models import storage
-#from models.user import User
-#from models.state import State
-#from models.city import City
-#from models.place import Place
-#from models.amenity import Amenity
-#from models.review import Review
+
+# from models.user import User
+# from models.state import State
+# from models.city import City
+# from models.place import Place
+# from models.amenity import Amenity
+# from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
-    __classes = {"BaseModel", "User", "State", "City", "Amenity", "Place", "Review"}
+    __classes = {"BaseM", "Usr", "St", "City", "Amenity", "Place", "Review"}
 
     def do_quit(self, arg):
         """Exit the program"""
