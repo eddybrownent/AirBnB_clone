cf9229116c3158529525a1c7f6dc76c1d13d7504
WinnyKay3 <winffredkalondu@gmail.com>
2023-07-17 11:53:37 -0700

    save changes

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,7 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of instance"""
-        return "[{}] ({}) {}".format(sef.__cls__.__nme__, sef.id, sef.__dc__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
